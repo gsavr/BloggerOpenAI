bec735c5bea9aa11c1fc416da2d2ed57b6b2c361
giorgio <giorgiosavron@me.com>
2023-03-06 10:50:13 -0500

    next.js imports

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Logo } from "./Logo/logo";
-import { LogoMobile } from "./Logo/logoMobile";
+import { Logo } from "../Logo";
+import { LogoMobile } from "../LogoMobile";
 
 export const NavBar = () => {
   return (
